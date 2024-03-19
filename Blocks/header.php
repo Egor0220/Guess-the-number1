519d4deeb8b9d0785cc6f5a8a78ab19a5d7ec4f5
Egor0220 <72023799+Egor0220@users.noreply.github.com>
2024-03-19 04:21:58 +0300

    Add files via upload

---
+++ Blocks/header.php
@@ -0,0 +1,15 @@
+htmp<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title><?=$title ?></title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+</head>
+<body>
+<header>
+    <a href="\Guess the number\game\home page.php">Начальная страница</a> |
+    <a href="\Guess the number\game\range select.php">Начать игру</a><br>
+</header>
